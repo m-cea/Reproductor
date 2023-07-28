b85c20b7f15dcd97164f0d4879d2004774130315
m-cea <mc1317@hotmail.com>
2023-07-28 17:10:04 -0300

    El login por google estaria funcionando

--- app/src/main/java/ar/edu/unsada/reproductor/Login.java
+++ app/src/main/java/ar/edu/unsada/reproductor/Login.java
@@ -1,14 +1,18 @@
 package ar.edu.unsada.reproductor;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -25,116 +29,77 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 9001;
-    private GoogleSignInClient mGoogleSignInClient;
-    private FirebaseAuth mAuth;
-    private Button atajo;
-    private TextView mRespuesta;
-    private SignInButton mBtnGoogle;
+        private static final int RC_SIGN_IN = 9001;
+        private GoogleSignInClient mGoogleSignInClient;
 
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-        // [START config_signin]
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("753967530369-4sfp4uoerilt3stle7h9bcftob9sbkkf.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
+            SignInButton btnSignInWithGoogle = findViewById(R.id.btn_google);
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        // [END config_signin]
+            // Configurar el clic del botón para iniciar sesión con Google
+            btnSignInWithGoogle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    signInWithGoogle();
+                }
+            });
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+            // Configura el cliente de inicio de sesión con Google
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
+            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        //Instanciar boton google
-        mBtnGoogle = findViewById(R.id.btn_google);
 
-        //Instanciar TextView
-        mRespuesta = findViewById(R.id.respuesta);
-
-        mBtnGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-
-    }
-
-    // [START signin]
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-    // [END signin]
-
-    private void updateUI(FirebaseUser user) {
-        user = mAuth.getCurrentUser();
-        if (user != null) {
-            irRadio();
         }
-    }
 
-    private void irRadio() {
-        Intent i = new Intent(Login.this, Tabs.class);
-        startActivity(i);
-        finish();
-    }
+        // Método para iniciar el inicio de sesión con Google
+        private void signInWithGoogle() {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
 
-    // [START on_start_check_user]
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-    // [END on_start_check_user]
+        // Procesar el resultado del inicio de sesión
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
 
-    // [START onactivityresult]
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                mRespuesta.setText(e.getMessage());
+            if (requestCode == RC_SIGN_IN) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    // Inicio de sesión con éxito, obtén la cuenta de Google
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account);
+                } catch (ApiException e) {
+                    // Error en el inicio de sesión con Google
+                    Log.w(TAG, "Error al iniciar sesión con Google: " + e.getStatusCode());
+                }
             }
         }
-    }
-    // [END onactivityresult]
 
-    // [START auth_with_google]
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            irRadio();
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            updateUI(null);
-                            mRespuesta.setText(task.getException().toString());
+        // Autenticar con Firebase usando la cuenta de Google
+        private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Inicio de sesión con Firebase exitoso, ir a la actividad de las pestañas (MainActivity)
+                                Intent intent = new Intent(Login.this, Tabs.class);
+                                startActivity(intent);
+                                finish(); // Opcional: finalizar la actividad de inicio de sesión para que no se pueda volver atrás
+                            } else {
+                                // Error en el inicio de sesión con Firebase
+                                Log.w(TAG, "Error al iniciar sesión con Firebase: " + task.getException());
+                                // Mostrar un mensaje de error, si es necesario
+                                Toast.makeText(Login.this, "Error en el inicio de sesión con Firebase", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+        }
     }
-// [END auth_with_google]
-}
--- app/src/main/java/ar/edu/unsada/reproductor/Tabs.java
+++ app/src/main/java/ar/edu/unsada/reproductor/Tabs.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
+
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Tabs extends AppCompatActivity {
 
     private Button cerrar;
     private TextView usuario;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,10 @@ public class Tabs extends AppCompatActivity {
 
         ViewPager viewPager = findViewById(R.id.viewPager);
         TabLayout tabLayout = findViewById(R.id.tabLayout);
+        cerrar = findViewById(R.id.btn_cerrar);
+        usuario = findViewById(R.id.nombreUsuario);
+
+        //Manejo de las tabs
 
         PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
 
@@ -40,5 +56,36 @@ public class Tabs extends AppCompatActivity {
 
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        // Inicializar FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Obtener el usuario actualmente conectado
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        //Saludo al usuario
+        usuario.setText("Bienvenid@ \n" + user.getDisplayName());
+
+        //Cerrar Sesion
+        cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
     }
+
+    public void logout() {
+        // Revocar el acceso de la aplicación a la cuenta de Google tal que la proxima vez que se inicia la app deba loguear otra vez
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build());
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                // Navegar de vuelta a la actividad de inicio de sesión
+                Intent intent = new Intent(Tabs.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
